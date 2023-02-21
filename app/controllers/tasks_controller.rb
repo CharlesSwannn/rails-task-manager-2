84ec17db2febfc02d0dcbd1f30f54e9e38bcf983
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-02-21 14:25:09 +0000

    refactored code and DRYed it

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,33 +1,42 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
+
   def show
-    @task = Task.find(params[:id])
   end
+
   def new
     @task = Task.new
   end
+
   def create
     task = Task.new(task_params)
     task.save
     redirect_to task_path(task)
   end
+
   def edit
-    @task = Task.find(params[:id])
   end
+
   def update
-    task = Task.find(params[:id])
-    task.update(task_params)
-    redirect_to task_path(task)
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
+
   def destroy
-    task = Task.find(params[:id])
-    task.delete
+    @task.delete
     redirect_to tasks_path
   end
 
   private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-  #create
-  get '/tasks/new', to: 'tasks#new', as: :new_task
-  post '/tasks', to: 'tasks#create'
-  #read
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show', as: :task
-  #update
-  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit
-  patch '/tasks/:id', to: 'tasks#update'
-  #delete
-  delete '/tasks/:id', to: 'tasks#destroy', as: :delete_task
+  resources :tasks
 end
